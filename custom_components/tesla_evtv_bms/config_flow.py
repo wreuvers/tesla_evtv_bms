0f86ee3c5789165cb13346fbbb121f89cc39dce6
wreuvers <80078939+wreuvers@users.noreply.github.com>
2025-04-18 20:41:34 -0400

    Update config_flow.py

--- custom_components/tesla_evtv_bms/config_flow.py
+++ custom_components/tesla_evtv_bms/config_flow.py
@@ -2,6 +2,15 @@ from homeassistant import config_entries
 import voluptuous as vol
 from .const import DOMAIN, CONF_NAME, CONF_PORT
 
+DATA_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME): str,
+    vol.Required(CONF_PORT): int,
+    vol.Required("pack_size"): float,
+    vol.Required("cells_in_series"): int,
+    vol.Required("min_cell_volts"): float,
+    vol.Required("max_cell_volts"): float,
+})
+
 class TeslaEVTVBMSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
@@ -11,8 +20,5 @@ class TeslaEVTVBMSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema({
-                vol.Required(CONF_NAME): str,
-                vol.Required(CONF_PORT): int,
-            })
+            data_schema=DATA_SCHEMA
         )
