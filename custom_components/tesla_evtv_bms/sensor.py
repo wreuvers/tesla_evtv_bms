49254b9d18141002aa13e4ab7b1c489573f5a36a
wreuvers <80078939+wreuvers@users.noreply.github.com>
2025-04-18 20:03:24 -0400

    Update sensor.py
    Modified to reduce the update rate from hundreds per second to 1 per second.

--- custom_components/tesla_evtv_bms/sensor.py
+++ custom_components/tesla_evtv_bms/sensor.py
@@ -1,3 +1,4 @@
+import time
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
@@ -5,21 +6,32 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN, SIGNAL_UPDATE_ENTITY
 
+# Define expected sensor types and their units
 SENSOR_TYPES = {
-    "state_of_charge": "%", "power": "W", "current": "A", "volts": "V",
-    "lowest_cell": "V", "highest_cell": "V", "average_cell": "V",
-    "max_cells": "", "active_cells": "", "freq_shift_volts": "V", "tcch_amps": "A"
+    "state_of_charge": "%",
+    "power": "W",
+    "current": "A",
+    "volts": "V",
+    "lowest_cell": "V",
+    "highest_cell": "V",
+    "average_cell": "V",
+    "max_cells": "",
+    "active_cells": "",
+    "freq_shift_volts": "V",
+    "tcch_amps": "A",
+    "raw_current": "A"
 }
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
     name = entry.data["name"].lower()
     coordinator = hass.data.setdefault(DOMAIN, {}).setdefault(name, {
-        "entities": {}, "values": {}
+        "entities": {},
+        "values": {}
     })
 
     async def add_or_update_entity(key):
         if key not in coordinator["entities"]:
-            unit = SENSOR_TYPES.get(key, None)
+            unit = SENSOR_TYPES.get(key)
             sensor = TeslaEvtvSensor(name, key, unit, coordinator)
             coordinator["entities"][key] = sensor
             async_add_entities([sensor])
@@ -40,6 +52,9 @@ class TeslaEvtvSensor(Entity):
         self._key = key
         self._unit = unit
         self._coordinator = coordinator
+        self._state = None
+        self._last_update = 0
+        self._cooldown = 1.0  # seconds between allowed updates
 
     @property
     def name(self):
@@ -51,7 +66,7 @@ class TeslaEvtvSensor(Entity):
 
     @property
     def state(self):
-        return self._coordinator["values"].get(self._key)
+        return self._state
 
     @property
     def unit_of_measurement(self):
@@ -65,3 +80,20 @@ class TeslaEvtvSensor(Entity):
             "manufacturer": "EVTV",
             "model": "Tesla BMS"
         }
+
+    async def async_added_to_hass(self):
+        async def handle_update(values):
+            if self._key in values:
+                now = time.monotonic()
+                if now - self._last_update >= self._cooldown:
+                    self._state = values[self._key]
+                    self._last_update = now
+                    self.async_write_ha_state()
+
+        self.async_on_remove(
+            async_dispatcher_connect(
+                self.hass,
+                SIGNAL_UPDATE_ENTITY.format(self._device),
+                handle_update
+            )
+        )
