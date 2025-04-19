0e8591ced327b357cb4ba697dd7888e9db84bd9e
wreuvers <80078939+wreuvers@users.noreply.github.com>
2025-04-18 20:52:52 -0400

    Update config_flow.py

--- custom_components/tesla_evtv_bms/config_flow.py
+++ custom_components/tesla_evtv_bms/config_flow.py
@@ -2,14 +2,11 @@ from homeassistant import config_entries
 import voluptuous as vol
 from .const import DOMAIN, CONF_NAME, CONF_PORT
 
-DATA_SCHEMA = vol.Schema({
-    vol.Required(CONF_NAME): str,
-    vol.Required(CONF_PORT): int,
-    vol.Required("pack_size"): float,
-    vol.Required("cells_in_series"): int,
-    vol.Required("min_cell_volts"): float,
-    vol.Required("max_cell_volts"): float,
-})
+DEFAULT_PORT = 6850
+DEFAULT_PACK_SIZE = 75.0
+DEFAULT_CELLS_SERIES = 96
+DEFAULT_MIN_VOLTS = 3.2
+DEFAULT_MAX_VOLTS = 4.1
 
 class TeslaEVTVBMSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
@@ -20,5 +17,15 @@ class TeslaEVTVBMSConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
         return self.async_show_form(
             step_id="user",
-            data_schema=DATA_SCHEMA
+            data_schema=vol.Schema({
+                vol.Required(CONF_NAME): str,
+                vol.Required(CONF_PORT, default=DEFAULT_PORT): vol.Coerce(int),
+                vol.Required("pack_size", default=DEFAULT_PACK_SIZE): vol.Coerce(float),
+                vol.Required("cells_in_series", default=DEFAULT_CELLS_SERIES): vol.Coerce(int),
+                vol.Required("min_cell_volts", default=DEFAULT_MIN_VOLTS): vol.Coerce(float),
+                vol.Required("max_cell_volts", default=DEFAULT_MAX_VOLTS): vol.Coerce(float),
+            }),
+            description_placeholders={
+                "info": "Configure the Tesla BMS listener"
+            }
         )
